4ba7929c1a2e547f07230ea2bc07e74f7bad8e15
Jayakumar V V <32296626+jk2552@users.noreply.github.com>
2021-01-25 22:05:43 +0530

    Update github_sample.c

--- github_sample.c
+++ github_sample.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 
 int main(){
-	
+	printf("\n");
 	printf("Hello world\n");
 	printf("Welcome to the jenkins automation\n");
+	printf("E-mail verification test\n");
 	printf("\n");
 	return 0;
 }
